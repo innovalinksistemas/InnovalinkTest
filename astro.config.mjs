cdf319a662ddd84992ad13e29e83b6bb036002b3
Wil <sevillawil8@gmail.com>
2024-06-07 16:33:04 -0600

    delete output sercer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wdsevilla.github.io',
-  base:'InnovalinkTest',
-  output: "server",
+  base:'/InnovalinkTest',
+  
   integrations: [tailwind()]
 });

b3573b56ea2b9914be209f24988183825b5910ab
EduardoPache <35412279+EduardoPache@users.noreply.github.com>
2025-03-17 22:45:44 -0600

    full site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wdsevilla.github.io',
-  base:'/InnovalinkTest',
+  image: {
+    service: passthroughImageService(),
+  },
   
   integrations: [tailwind()]
 });

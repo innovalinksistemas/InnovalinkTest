b796bf7c1046c734110284a9343cc7b048a67cdc
Sistemas <sistemas@innovalinkcr.com>
2025-05-22 15:48:39 -0600

    change logo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,14 @@ export default {
   theme: {
     extend: {
       animation: {
-        lightline: 'lightline 2s ease-in-out infinite',
+        lightline: 'lightline 2s ease-in-out infinite' ,
+        'pulse-glow': 'pulseGlow 3s ease-in-out infinite',
       },
       keyframes: {
+        pulseGlow: {
+          '0%, 100%': { filter: 'drop-shadow(0 0 15px rgba(242,247,253,0.3))' },
+          '50%': { filter: 'drop-shadow(0 0 25px rgba(242,204,255,0.9))' },
+        },
         lightline: {
           '0%': { opacity: 0.2, transform: 'translateX(-10px)' },
           '50%': { opacity: 1, transform: 'translateX(10px)' },
